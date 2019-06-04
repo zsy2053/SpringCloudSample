285c6d26b5d2d2f61e2aa21b5282708109b4f9fe
Shiquan Zang <zsy2053@Shiquans-MacBook-Pro.local>
2019-06-03 23:34:30 -0400

    first commit

---
+++ src/main/java/com/example/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.eurekaservice;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
